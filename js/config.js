4394cffe31a309d1b6dfe4cb2904a61c05483445
Jerson <112001813+JersonUCB@users.noreply.github.com>
2026-09-15 13:04:29 -0400

    chore: initialize static weather app

---
+++ js/config.js
@@ -0,0 +1,15 @@
+export const cities = [
+  { name: "Sucre", latitude: -19.0333, longitude: -65.2627 },
+  { name: "La Paz", latitude: -16.5, longitude: -68.15 },
+  { name: "Cochabamba", latitude: -17.3895, longitude: -66.1568 },
+  { name: "Oruro", latitude: -17.9833, longitude: -67.15 },
+  { name: "Potosí", latitude: -19.5836, longitude: -65.7533 },
+  { name: "Tarija", latitude: -21.5355, longitude: -64.7296 },
+  {
+    name: "Santa Cruz de la Sierra",
+    latitude: -17.8146,
+    longitude: -63.1561,
+  },
+  { name: "Trinidad", latitude: -14.8333, longitude: -64.9 },
+  { name: "Cobija", latitude: -11.0333, longitude: -68.7333 },
+];
